41722a9b89f42ee9aea8ce4203fbd1aac17155bf
Dostonjon-98 <mahmanazarovdoston98@gmail.com>
2023-05-12 03:38:04 +0500

    ui package changed

--- app/src/main/java/uz/uzapexsoft/cleanarchitecture/application/App.kt
+++ app/src/main/java/uz/uzapexsoft/cleanarchitecture/application/App.kt
@@ -1,4 +1,4 @@
-package uz.uzapexsoft.cleanarchitecture.presentation.application
+package uz.uzapexsoft.cleanarchitecture.application
 
 import android.app.Application
 import org.koin.android.ext.koin.androidContext
